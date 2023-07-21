b0d51c71a4883f1b8290adec10fb23ccfd97f81c
Mitchellindev <mitchellin.tessy@gmail.com>
2023-07-21 14:17:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,90 @@
+// arrays
+
+let mixedArray = [1, "apple", [1, 2, 3], true];
+console.log(mixedArray);
+
+// object
+
+const user = {
+	name: "chelly",
+	age: 16,
+	"favourite food": "fried rice",
+	"shoe size": 38,
+	"skin tone": "chocolate",
+};
+
+console.log(user);
+
+// operators
+// math operator(+ - / * % **)
+
+let addition = 2 + 2;
+let multiplication = 2 * 2;
+
+// assignment operator (=)
+//comparison operator (> < == != >=)
+
+console.log(2 == 2);
+console.log(10 != 2);
+console.log(!true);
+console.log("1" == 1);
+
+// conditional statement
+const age = 16;
+if (age == 18) {
+	console.log(adult);
+} else {
+	console.log("under age");
+}
+
+// ternery operator
+isMarried = false;
+
+isMarried == true ? console.log("I am married") : console.log("single");
+
+// string concatination
+
+let firstName = "Hope";
+let lastName = "Chelly";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+// function
+// function declaration
+function sum(num1, num2) {
+	return num1 + num2;
+}
+
+sum(2, 3);
+
+let add = sum(2, 8);
+console.log(add);
+
+// strings and string method
+
+let userName = "mitchell";
+console.log(userName.length);
+
+console.log(typeof userName);
+
+console.log(userName.toUpperCase());
+
+console.log(userName[2]);
+
+console.log(userName.indexOf("e"));
+
+
+// slice
+console.log(userName.slice(0, 5));
+
+let str = 'i love javascript';
+
+str.lastIndexOf('javascript').toUpperCase
+
+console.log(str);
+
+
+
+
